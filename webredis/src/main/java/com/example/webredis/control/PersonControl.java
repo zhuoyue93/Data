568c361af8c1c8f2112dd7e05c872612fc9e05b5
zhuoyuec <zhuoyuec@yonyou.com>
2019-09-11 13:47:05 +0800

    存储人员全量数据到redis

--- webredis/src/main/java/com/example/webredis/control/PersonControl.java
+++ webredis/src/main/java/com/example/webredis/control/PersonControl.java
@@ -3,12 +3,19 @@ package com.example.webredis.control;
 import com.example.webredis.data.BdPsndoc;
 import com.example.webredis.data.BdPsndocExample;
 import com.example.webredis.data.BdPsndocMapper;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 public class PersonControl {
@@ -16,12 +23,18 @@ public class PersonControl {
     private StringRedisTemplate redisTemplate;
     @Autowired
     private BdPsndocMapper userMapper;
+
+    private static final String DATA="data";
     @RequestMapping("/getUser")
-    public void getUser() {
+    public void getUser() throws JsonProcessingException {
         List<BdPsndoc> userEntities = userMapper.selectByExample(new BdPsndocExample());
         for (BdPsndoc userEntity : userEntities) {
-            redisTemplate.opsForValue().set("11",userEntity.toString());
-            System.out.println(redisTemplate.opsForValue().get("11"));
+            redisTemplate.opsForHash().put(DATA,userEntity.getPkPsndoc(), new ObjectMapper().writeValueAsString(userEntity));
+        }
+        Set<Object> data = redisTemplate.opsForHash().keys(DATA);
+        for (Object datum : data) {
+            Object o = redisTemplate.opsForHash().get(DATA, datum);
+            System.out.println(o);
         }
     }
 }
